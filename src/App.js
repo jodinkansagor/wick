c5cc279cd2ee7b99ac915cf1f3e8cb827a635d88
jodinkansagor <jodikansagor@Jodis-MacBook-Air.local>
2022-04-23 11:48:38 -0700

    routing set up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import { ThemeProvider } from '@mui/material/styles';
 import theme from "./theme/theme"
-import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import Home from './components/Home';
+import About from './components/About';
+import Coaching from './components/Coaching';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <div>
-        <Home />
+        <Router>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/coaching" element={<Coaching />} />
+            <Route path="/contact" element={<Contact />} />
+
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
       </div>
     </ThemeProvider>
   )
 }
 
 export default App;
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
+
 import Typography from '@mui/material/Typography';
-import heroImage from "../assets/image2.jpeg"
-import "./style.scss"
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
+
+import { Link } from "react-router-dom"
+
+import "./style.scss"
 
 
 const Home = () => {
@@ -15,12 +17,23 @@ const Home = () => {
   const toggleDrawer = () => {
     setOpenDrawer(!openDrawer)
   }
+
   return (
     <div className="HeroContainer">
       <div className="HeroContainer-title">
         <div>
           <Typography variant="h3" color="secondary">Wick Lawson</Typography>
           <Typography variant="h3" color="secondary">Coaching</Typography>
+        </div>
+        <div className="HeroContainer-menu">
+          <Link to="/about">
+            <Typography color="secondary">About</Typography></Link>
+          <Link to="/Coaching">
+            <Typography color="secondary">Coaching</Typography>
+          </Link>
+          <Link to="/contact">
+            <Typography color="secondary">Contact</Typography>
+          </Link>
         </div>
       </div>
       <div className="HeroContainer-buttonAndDrawer">
@@ -36,17 +49,17 @@ const Home = () => {
           <div className="HeroContainer-drawerContent">
             <IconButton onClick={toggleDrawer} size='large'><CloseIcon /></IconButton>
             <ul>
-              <li>About</li>
-              <li>Coaching</li>
-              <li>Contact</li>
+              <Link to="/about"><li>About</li></Link>
+              <Link to="/coaching"><li>Coaching</li></Link>
+              <Link to="/contact"><li>Contact</li></Link>
             </ul>
           </div>
         </Drawer>
       </div>
+
       <div className="HeroContainer-quote" >
         <Typography variant="h4" color="secondary">"Growth flows from our ability to accept fully.”</Typography>
       </div>
-      {/* <img className="HeroContainer-wickImage" src={heroImage} alt='Wick Lawson' /> */}
     </div>
   )
 }
